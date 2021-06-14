fd8f29b4129acedc352169e070250286309ff4d2
dnakazima <dnakazima@hotmail.com>
2021-06-14 10:51:26 -0300

    criado classe Program Product ordeItem

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,33 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.OrderItem;
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product p = new Product("TV", 1000.00);
+		
+		OrderItem oi1 = new OrderItem(1, 1000.00, p);
+		
+		System.out.println(oi1.getQuantity());
+		System.out.println(oi1.getProduct().getName());
+				
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
 
 	}
 
